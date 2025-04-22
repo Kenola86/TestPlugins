ac1910898ce6d3888c8d57b6006cdbfba7cc6747
Kenola86 <cckenola86@gmail.com>
2025-04-22 21:10:47 +0200

    Update ExamplePlugin.kt

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -4,7 +4,7 @@ import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
-class Motherless : MainAPI() {
+class ExamplePlugin : MainAPI() {
     override var mainUrl              = "https://motherless.com"
     override var name                 = "Motherless"
     override val hasMainPage          = true
